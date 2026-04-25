dffda8327107119e3560aa404587b79ec8fd59bc
SangMzG <levansang16102009ii@gmail.com>
2026-04-25 11:12:57 +0700

    Update windows.bat

--- build/windows.bat
+++ build/windows.bat
@@ -4,6 +4,23 @@ haxelib --global install hmm
 haxelib --global run hmm setup
 hmm install
 haxelib run lime setup
+
+haxelib install flixel
+haxelib install flixel-addons
+haxelib install flixel-animate
+haxelib install polymod
+haxelib install json2object
+haxelib install jsonpath
+haxelib install jsonpatch
+haxelib install thx.core
+haxelib install thx.semver
+
+haxelib install hxvlc
+haxelib install hxdiscord_rpc
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis
+haxelib install grig.audio
+haxelib install newgrounds
+
 lime rebuild windows
 lime rebuild windows -debug
 lime test windows
