07ad0b14c60ee0f8c2e33ec61b06ea6000ba310b
SangMzG <levansang16102009ii@gmail.com>
2026-04-25 11:38:39 +0700

    Modify windows.bat for Haxelib updates and cleanup
    Updated the Windows build script to include additional Haxelib installations and removed debug build command.

--- build/windows.bat
+++ build/windows.bat
@@ -1,27 +1,36 @@
 @echo off
+
 git submodule update --init --recursive
+
 haxelib --global install hmm
 haxelib --global run hmm setup
 hmm install
+
+haxelib install lime
 haxelib run lime setup
 
 haxelib install flixel
 haxelib install flixel-addons
 haxelib install flixel-animate
-haxelib install polymod
+
 haxelib install json2object
 haxelib install jsonpath
 haxelib install jsonpatch
+
 haxelib install thx.core
 haxelib install thx.semver
 
+haxelib install hxcpp
 haxelib install hxvlc
 haxelib install hxdiscord_rpc
-haxelib git funkin.vis https://github.com/FunkinCrew/funkVis
+
 haxelib install grig.audio
 haxelib install newgrounds
+haxelib install polymod
+
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis
 
 lime rebuild windows
-lime rebuild windows -debug
 lime build windows
+
 pause
